3a6ee9d4bfa1aad7d37e3e32744cba43769dbcc6
Andrey Osintsev <ozzy.ext@gmail.com>
2020-02-26 00:21:06 +0300

    Move dependencies from MyLab to MyLab.Logging and remove MyLab reference

---
+++ src/MyLab.Logging/ExceptionCondition.cs
@@ -0,0 +1,21 @@
+﻿namespace MyLab.Logging
+{
+    /// <summary>Contains condition for exception</summary>
+    public class ExceptionCondition
+    {
+        /// <summary>Key</summary>
+        public string Key { get; }
+
+        /// <summary>Value</summary>
+        public object Value { get; }
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="ExceptionCondition" />
+        /// </summary>
+        public ExceptionCondition(string key, object value)
+        {
+            Key = key;
+            Value = value;
+        }
+    }
+}
