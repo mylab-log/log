1d50041581006d6852ff0dd007819007045f11c9
Andrey Osintsev <ozzy.ext@gmail.com>
2020-09-17 13:53:25 +0300

    #2 Add ExceptionExtensions tests

---
+++ src/MyLab.Logging.Tests/ExceptionExtensionsBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace MyLab.Logging.Tests
+{
+    public class ExceptionExtensionsBehavior
+    {
+        [Fact]
+        public void ShouldProvideConditions()
+        {
+            //Arrange
+            var e = new Exception();
+            e.AndFactIs("foo", "bar");
+
+            //Act
+            var conditions = e
+                .GetConditions()
+                .ToArray();
+
+            //Assert
+            Assert.Single(conditions);
+            Assert.Equal("foo", conditions.First().Key);
+            Assert.Equal("bar", conditions.First().Value);
+        }
+    }
+}
