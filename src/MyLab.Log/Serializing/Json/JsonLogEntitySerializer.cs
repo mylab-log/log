ca68cdac40a6b4b351cbb57ef4fdda579fb2bea9
Andrey O <ozzy.ext@gmail.com>
2022-06-20 01:25:50 +0300

    Compact logging exception log dump

--- src/MyLab.Log/Serializing/Json/JsonLogEntitySerializer.cs
+++ src/MyLab.Log/Serializing/Json/JsonLogEntitySerializer.cs
@@ -1,4 +1,6 @@
-﻿using Newtonsoft.Json;
+﻿using System.IO;
+using System.Text;
+using Newtonsoft.Json;
 
 namespace MyLab.Log.Serializing.Json
 {
@@ -10,8 +12,9 @@ namespace MyLab.Log.Serializing.Json
     {
         public string Serialize(LogEntity logEntity)
         {
-            return JsonConvert.SerializeObject(logEntity, Formatting.Indented, new JsonSerializerSettings
+            var serializer = new JsonSerializer
             {
+                Formatting = Formatting.Indented,
                 NullValueHandling = NullValueHandling.Ignore,
                 ContractResolver = new LogContractResolver(),
                 Converters =
@@ -20,14 +23,24 @@ namespace MyLab.Log.Serializing.Json
                     new ReflectionConverter(),
                     new JTokenConverter()
                 },
-                Error = (sender, args) =>
-                {
-                    args.ErrorContext.Handled = true;
-
-                },
                 DateTimeZoneHandling = DateTimeZoneHandling.Local,
                 DateFormatString = "yyyy-MM-ddTHH:mm:ss.fff"
-            });
+            };
+
+            serializer.Error += (sender, args) =>
+            {
+                args.ErrorContext.Handled = true;
+            };
+
+            var sb = new StringBuilder();
+
+            using (var tetWriter = new StringWriter(sb) { NewLine = "\n" })
+            using (var jsonWriter = new JsonTextWriter(tetWriter))
+            {
+                serializer.Serialize(jsonWriter, logEntity);
+            }
+
+            return sb.ToString();
         }
     }
 }
